5c79ee9f7a2be145861aa1b9f04320b005ee848a
annisaard <annisarahmadhani78@gmail.com>
2024-01-24 23:00:34 +0700

    chore(packages): add plugin tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,15 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
 };
 export default config;
